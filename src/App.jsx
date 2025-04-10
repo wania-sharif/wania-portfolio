3a69d98faf3b26f4ede1592a703811ec616bc147
Wania Sharif <w0842810@myscc.ca>
2025-04-10 19:20:34 -0400

    Create Projects page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './components/Home/Home'
+import Projects from './components/Projects/Projects'
 
 function App() {
 
   return (
     <div className='App'>
       <Home />
+      <Projects />
     </div>
   )
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import './projects.css'
+
+function Projects() {
+
+  return (
+    <div className='projects'>
+        <h3>PROJECTS</h3>
+        <div className='content'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Projects
