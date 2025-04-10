2c42c4a1939a2df13d48db0a1e6462ee11d766a6
Wania Sharif <w0842810@myscc.ca>
2025-04-09 22:56:59 -0400

    #1 style name and title, modify default styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,8 +3,9 @@ import './Home.css'
 function Home() {
 
   return (
-    <div className='Home'>
-      <h1>Wania</h1>
+    <div className='home'>
+      <h1>Wania S.</h1>
+      <h2>SOFTWARE DEVELOPER</h2>
     </div>
   )
 }
