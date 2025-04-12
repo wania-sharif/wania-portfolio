69bc83138085001b963687882cc86d0f28e8b5b0
Wania Sharif <w0842810@myscc.ca>
2025-04-11 23:55:53 -0400

    #3 Create horizontal scrolling panel to display project cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,24 @@
 import ProjectCard from './ProjectCard/ProjectCard'
 import './projects.css'
+import { motion, useTransform, useScroll } from "framer-motion";
 
 function Projects() {
+  const { scrollYProgress } = useScroll();  // track scroll progress
+
+  // Map scroll position values
+  const x = useTransform(scrollYProgress, [0, 1], ["60%", "-90%"]);
 
   return (
     <div className='projects'>
-        <h3>PROJECTS</h3>
-        
         <div className='content'>
-            <ProjectCard />
+          <h3>PROJECTS</h3>
+
+          <motion.div style={{ x }} className='card-container'>
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+          </motion.div>
         </div>
     </div>
   )
