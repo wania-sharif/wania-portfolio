74f90193678725c409945bccbf89c8a40b238e37
Wania Sharif <w0842810@myscc.ca>
2025-04-19 13:37:23 -0400

    #2 create projects data array

---
+++ src/assets/data/projects.js
@@ -0,0 +1,34 @@
+const projects = [
+    {
+        name: 'Renova',
+        description: 'E-commerce platform for a clothing line',
+        images: [
+            '/images/placeholder.png',
+            '/images/placeholder.png',
+            '/images/placeholder.png'],
+        tech: ['ReactJS', 'NodeJS', 'ExpressJS'],
+        link: ''
+    },
+    {
+        name: 'Expense Tracker',
+        description: 'App to track your expenses',
+        images: [
+            '/images/placeholder.png',
+            '/images/placeholder.png',
+            '/images/placeholder.png'],
+        tech: ['Java', 'JDBC', 'IntelliJ'],
+        link: ''
+    },
+    {
+        name: 'The Easiest Game',
+        description: 'A simple game to play...',
+        images: [
+            '/images/placeholder.png',
+            '/images/placeholder.png',
+            '/images/placeholder.png'],
+        tech: ['Java', 'JavaFX', 'IntelliJ'],
+        link: ''
+    },
+]
+
+export default projects
