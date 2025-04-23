5f8bf16e308b4fb8bb6708a5b5fe7cc60bbdd349
Wania Sharif <w0842810@myscc.ca>
2025-04-23 11:47:56 -0400

    create contact page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Projects from './components/Projects/Projects'
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Home />
       <Projects />
+      <Contact />
     </div>
   )
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contact.css';
+
+const Contact = () => {
+    return (
+        <div className="contact" id="contact">
+            <h3>CONTACT ME</h3>
+        </div>
+    );
+};
+
+export default Contact;
