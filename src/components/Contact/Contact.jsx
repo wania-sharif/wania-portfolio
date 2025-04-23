8a59db5aa6acf93e4e0f77644e36605e2ff17ab5
Wania Sharif <w0842810@myscc.ca>
2025-04-23 16:08:45 -0400

    #7 create contact page layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,6 +5,23 @@ const Contact = () => {
     return (
         <div className="contact" id="contact">
             <h3>CONTACT ME</h3>
+            <div>
+                <section className="socials">
+                    <h2>SOCIAL</h2>
+                    <ul>
+                        <li>LinkedIn</li>
+                        <li>GitHub</li>
+                        <li>Instagram</li>
+                    </ul>
+                </section>
+                <section className="contacts">
+                    <h2>CONTACT</h2>
+                    <ul>
+                        <li>E-mail</li>
+                        <li>Whatsapp</li>
+                    </ul>
+                </section>
+            </div>
         </div>
     );
 };
