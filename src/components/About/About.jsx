44e5167ac1170d6f0ab506e666fa048a5895fc69
Wania Sharif <w0842810@myscc.ca>
2025-04-30 13:24:38 -0400

    create paragraph and career sections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,6 +4,23 @@ const About = () => {
     return (
         <div className="about">
             <h2>ABOUT ME</h2>
+            <div className='content'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris sit amet lectus vel magna tincidunt commodo. Pellentesque
+                    tincidunt nec lectus vel fermentum. Vivamus leo lectus, porttitor
+                    a fringilla non, ultrices pulvinar eros. Donec at tincidunt mi. 
+                    Quisque ac finibus nulla. Orci varius natoque penatibus et magnis 
+                    dis parturient montes, nascetur ridiculus mus. Nam a lorem congue, 
+                    pellentesque erat ultrices, porttitor dolor. Nullam sit amet lorem 
+                    ultricies, cursus sapien at, consectetur magna.
+                </p>
+
+                <div className='career'>
+                    <h3>EXPERIENCE</h3>
+                    <h3>EDUCATION</h3>
+                    
+                </div>
+            </div>
         </div>
     );
 }
