ed11ef6116c443b3edf1e9ac5d2f530bc53ac4e1
Wania Sharif <w0842810@myscc.ca>
2025-04-30 18:31:32 -0400

    #11 add awards section, insert experience

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,13 +3,10 @@ import './about.css';
 const About = () => {
     return (
         <div className="about">
-            <h2>ABOUT ME</h2>
+            {/* <h2>ABOUT ME</h2> */}
             <div className='content'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Mauris sit amet lectus vel magna tincidunt commodo. Pellentesque
-                    tincidunt nec lectus vel fermentum. Vivamus leo lectus, porttitor
-                    a fringilla non, ultrices pulvinar eros. Donec at tincidunt mi. 
-                    Quisque ac finibus nulla. Orci varius natoque penatibus et magnis 
+                <p>Hey, I'm Wania Sharif. I am a software developer based in Windsor, Ontario <br/>
+                    <br/>Quisque ac finibus nulla. Orci varius natoque penatibus et magnis 
                     dis parturient montes, nascetur ridiculus mus. Nam a lorem congue, 
                     pellentesque erat ultrices, porttitor dolor. Nullam sit amet lorem 
                     ultricies, cursus sapien at, consectetur magna.
@@ -18,19 +15,32 @@ const About = () => {
                 <div className='career'>
                     <h3>EXPERIENCE</h3>
                     <Item
-                        title="MOBILE APPLICATION DEVELOPMENT"
-                        date="2023 - 2026"
-                        description="St. Clair College, Windsor, Ontario"
+                        title="CICAN Hackathon"
+                        date="2025"
+                        description="Hackathon hosted by Colleges and Institutes of Canada"
                     />
                     <Item
-                        title="MOBILE APPLICATION DEVELOPMENT"
-                        date="2023 - 2026"
-                        description="St. Clair College, Windsor, Ontario"
+                        title="WinHacks"
+                        date="2025"
+                        description="Hackathon hosted by University of Windsor"
                     />
+                    <Item
+                        title="Software Development Student"
+                        date="2023 - NOW"
+                        description="Learning practices and principles of software development through
+                        the development of full stack applications."
+                    />
+
+                    <h3>AWARDS</h3> 
+                    <ul>
+                        <li>Academic Excellence Scholarship x2</li>
+                        <li>Academic Distinction Award x3</li>
+                    </ul>
+                    
 
                     <h3>EDUCATION</h3>
                     <Item
-                        title="MOBILE APPLICATION DEVELOPMENT"
+                        title="Mobile Application Development, AA/OCAD"
                         date="2023 - 2026"
                         description="St. Clair College, Windsor, Ontario"
                     />
