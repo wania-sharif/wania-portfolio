f380ce53d82dfdbcbe3161de0707829e9c00e8fd
Wania Sharif <w0842810@myscc.ca>
2025-05-01 12:08:28 -0400

    #7 reduce 2 sections to 1, add copyright text

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,30 +11,20 @@ const Contact = () => {
             initial={{ opacity: 0, y: 50 }}
             animate={ isInView && { opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            >CONTACT ME</motion.h3>
+            >Let's chat</motion.h3>
 
-            <div>
-                <motion.div className="socials"
-                    initial={{ opacity: 0, y: -50 }}
-                    animate={ isInView && { opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5, delay: 0.5 }}>
-                    <h2>SOCIAL</h2>
-                    <ul>
-                        <li><a href="https://www.linkedin.com/in/wania-sharif-a76834324" target='_blank'>LinkedIn</a></li>
-                        <li><a href="https://github.com/wania-sharif" target='_blank'>GitHub</a></li>
-                    </ul>
-                </motion.div>
-                
-                <motion.div className="contacts"
+            <motion.div className="socials"
                 initial={{ opacity: 0, y: -50 }}
-                animate={isInView && { opacity: 1, y: 0 }}
-                transition={{ duration: 0.4, delay: 0.6 }}>
-                    <h2>CONTACT</h2>
-                    <ul>
-                        <li><a href="mailto:someone@example.com">E-mail</a></li>
-                    </ul>
-                </motion.div>
-            </div>
+                animate={ isInView && { opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.5 }}>
+                <ul>
+                    <li><a href="mailto:someone@example.com">E-mail</a></li>
+                    <li><a href="https://www.linkedin.com/in/wania-sharif-a76834324" target='_blank'>LinkedIn</a></li>
+                    <li><a href="https://github.com/wania-sharif" target='_blank'>GitHub</a></li>
+                </ul>
+            </motion.div>
+
+            <p>© 2025 Wania Sharif</p>
         </div>
     );
 };
